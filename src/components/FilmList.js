5531df86abaa3b3c30d2534782a59e6d64e997b6
Andrew High <andrewhigh4@hotmail.com>
2021-05-11 07:31:58 +0100

    mvp complete

---
+++ src/components/FilmList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Film from './Film';
+
+const FilmList = ({films}) => {
+    const filmNodes = films.map(film => {
+        return (
+            <Film key={film.id} name={film.name} url={film.url}></Film>
+        );
+    });
+    return(
+        <>
+            <ul>{filmNodes}</ul>
+        </>
+    )
+};
+
+export default FilmList;
---
+++ src/components/UpcomingReleases.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const UpcomingReleases = () => {
+    return(
+    <a href= 'https://www.imdb.com/calendar/?region=gb'>View more upcoming releases>></a>
+    )
+}
+
+export default UpcomingReleases;
